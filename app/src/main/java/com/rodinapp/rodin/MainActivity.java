ccf6489e068bd43487b66340d399b5c44b406b85
serghov <sergey.hovakimyan@gmail.com>
2016-10-12 16:23:33 +0400

    initial commit

---
+++ app/src/main/java/com/rodinapp/rodin/MainActivity.java
@@ -0,0 +1,43 @@
+package com.rodinapp.rodin;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+               return false;
+            }
+        });
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(getResources().getString(R.string.url));
+    }
+}
